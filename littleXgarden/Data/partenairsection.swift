4a9927453495d05e56b237629018ed89bf0a209c
hydroft1 <hydroftcount@hotmail.com>
2023-06-05 06:38:24 +0200

    home Page presque fini,  commencement de la page intérieur

---
+++ littleXgarden/Data/partenairsection.swift
@@ -0,0 +1,20 @@
+//
+//  partenairsection.swift
+//  littleXgarden
+//
+//  Created by Alexandre Marquet on 05/06/2023.
+//
+
+import SwiftUI
+
+struct partenairsection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct partenairsection_Previews: PreviewProvider {
+    static var previews: some View {
+        partenairsection()
+    }
+}
