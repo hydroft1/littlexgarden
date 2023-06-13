c430b911763c90c62c5161ff931f2a5a25ad44d2
hydroft1 <hydroftcount@hotmail.com>
2023-06-13 16:47:12 +0200

    created data file image

---
+++ littleXgarden/Data/data.swift
@@ -0,0 +1,14 @@
+// created by hydroft
+import Foundation
+
+
+struct imageCurrency {
+    let name: String
+    let iconName: String
+    
+}
+
+let imagelist = [
+    imageCurrency(name: "one piece", iconName: "one-piece"),
+    imageCurrency(name: "bleach", iconName: "bleach")
+]
